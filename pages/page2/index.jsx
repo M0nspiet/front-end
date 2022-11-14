0d457c2490346a44b343a155ba67349640efe589
M0nspiet <Kaliberovnik04@mail.ru>
2022-11-15 00:56:03 +0300

    idz_3

---
+++ pages/page2/index.jsx
@@ -0,0 +1,12 @@
+import { abz } from "../../components/p"
+
+export default function p2Page(){
+    return (
+    <main>
+        <abz>page 2</abz>
+        <div>
+        <abz>Here is abz #2</abz>
+        </div>
+    </main>
+    );
+}
