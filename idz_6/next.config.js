6c0e730e3b9354a2a03a02b8269c8c74f946023b
M0nspiet <Kaliberovnik04@mail.ru>
2022-12-04 03:40:24 +0300

    dz 6 done

---
+++ idz_6/next.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  //images :{
+    //loader : "M0nspiet",
+    //path: "",
+  //},
+
+}
+
+module.exports = nextConfig
