946f899d49125a66b1bfc27b69f204fcbd7e19c3
M0nspiet <Kaliberovnik04@mail.ru>
2022-12-04 03:36:33 +0300

    del 6

--- idz_6/components/Header/UI/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import css from "./Button.module.css"
-import { clx } from "../../../../untils/clx"
-
-export const Button = ({children, className, ...props}) => {
-    return(
-    <button className={clx(css.button)} {...props}>{children}</button>
-    )
-} 
